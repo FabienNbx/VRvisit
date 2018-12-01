aeb6fc0addccf03edab5ce60f262eb4af97c85df
FabienNbx <narbouxfabien42@hotmail.fr>
2018-12-01 10:42:41 +0100

    maj

--- AframeTour/360tour.js
+++ AframeTour/360tour.js
@@ -9,11 +9,20 @@ AFRAME.registerComponent('move', {
 		var el=this.el;
 		var target=document.querySelector(`#${data.target}`)
 		el.addEventListener(data.on, function(){
-				//if img existe charger image
-				//else creer img avec id asset item et source innerHTML
+				var image=document.querySelector(`#${data.target}Img`);
+				if(image.nodeName!="IMG"){
+					var source=image.innerHTML;
+					var parentImage=image.parentNode;
+					parentImage.removeChild(image);
+					image=document.createElement("img");
+					image.setAttribute("id", `${data.target}Img`);
+					image.setAttribute("crossorigin", "anonymous");
+					image.setAttribute("src", source);
+					parentImage.appendChild(image);
+				}
 				document.querySelector("#background").setAttribute('src', `#${data.target}Img`);
 				target.setAttribute('visible', 'true');
-				el.parentNode.setAttribute('visible','false');
+				el.parentNode.parentNode.setAttribute('visible','false');
 				document.querySelector("#hud").setAttribute('value', target.getAttribute('description'));
 				document.querySelector("#cursor").setAttribute('raycaster', `objects: #${data.target}`);
 			}
@@ -27,6 +36,15 @@ AFRAME.registerComponent('default', {
 
 	init: function(){
 		var el = this.el;
+		var image=document.querySelector(`#${el.id}Img`);
+		var source=image.innerHTML;
+		var parentImage=image.parentNode;
+		parentImage.removeChild(image);
+		image=document.createElement("img");
+		image.setAttribute("id", `${el.id}Img`);
+		image.setAttribute("crossorigin", "anonymous");
+		image.setAttribute("src", source);
+		parentImage.appendChild(image);
 		document.querySelector("#background").setAttribute('src', `#${el.id}Img`);
 		document.querySelector("#hud").setAttribute('value', el.getAttribute('description'));
 		el.setAttribute('visible', 'true');
@@ -39,6 +57,3 @@ AFRAME.registerComponent('description', {
 	}
 );
 
-function clickSky(){
-	alert("click");
-}
