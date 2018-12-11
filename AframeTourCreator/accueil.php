3b3a1e3bae9829cba67f40b42b36cd29ac80897a
EnkiGs <enki.garrigues@free.fr>
2018-12-11 19:41:17 +0100

    Revert "Creator CSS OK"
    This reverts commit 4c70b9a7e793518817f9737dd02503b27cec8a0e.

--- AframeTourCreator/accueil.php
+++ AframeTourCreator/accueil.php
@@ -6,27 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Aframe Tour Creator</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="css/accueil.css" />
+    <link type="text/css" rel="stylesheet" href="css/tourCreator.css" />
 </head>
-<body class="bg-info">
+<body>
 	<div><h1>Voici la liste des photos de votre tour 360° :</h1></div>
-	<section id="listeImg" class="text-center">
-		<?php
-			if($dossier = opendir('./uploads')){
-				while(false !== ($fichier = readdir($dossier)))
-				{
-					if($fichier != '.' && $fichier != '..' && $fichier != 'ajout.jpeg' && $fichier != 'validation.png')
-					{
-						echo "<img class=\"photos rounded img-fluid\" src=\"./uploads/".basename($fichier)."\" alt=\"Désolé notre image a rencontré des problèmes\">";
-						
-					}
-				}
-			}
-		?>
+	<section id="listeImg">
+		<script type="text/javascript" src="js/gestionImages.js"></script>
 	</section>
 	<footer>
-	<div class="d-flex justify-content-center">
-		<button class="btn btn-danger">Créer</button>
+	<div class="container-fluid">
+		<script type="text/javascript">
+			<?php
+				if($dossier = opendir('./uploads')){
+					while(false !== ($fichier = readdir($dossier)))
+					{
+						if($fichier != '.' && $fichier != '..' && $fichier != 'ajout.jpeg' && $fichier != 'validation.png')
+						{
+							$ext = ".".strtolower(pathinfo($fichier,PATHINFO_EXTENSION));
+							echo "ajoutImage('".basename($fichier,$ext)."','uploads/".$fichier."');";
+							
+						}
+					}
+				}
+			?>
+		</script>
+		<button id="Ajout" class="btn btn-primary" onclick="ajoutDossier()">
+			<img class="imgBtn" src="images/ajout.jpeg" alt="Désolé notre image a rencontré des problèmes">
+		</button>
+
+		<button class="btn btn-success">
+			<img class="imgBtn" src="images/validation.png" alt="Désolé notre image a rencontré des problèmes">
+		</button>
 	</div>	
 	</footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
