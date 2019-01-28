6e1631fca8fac03fc787cf751c44f8b465c30da7
FabienNbx <narbouxfabien42@hotmail.fr>
2019-01-28 12:05:18 +0100

    add pann + point ok

--- AframeTourCreator/accueil.php
+++ AframeTourCreator/accueil.php
@@ -6,37 +6,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Aframe Tour Creator</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="tourCreator.css" />
+    <link type="text/css" rel="stylesheet" href="css/accueil.css" />
 </head>
-<body>
+<body class="bg-info">
 	<div><h1>Voici la liste des photos de votre tour 360° :</h1></div>
-	<section id="listeImg">
-		<script type="text/javascript" src="js/gestionImages.js"></script>
-	</section>
-	<footer>
-	<div class="container-fluid">
-		<script type="text/javascript">
-			<?php
+	<section id="listeImg" class="text-center">
+		<?php
+			//setcookie("testcook","", time()-3600);
+			//$cpt=0;
+			if($dossier = opendir('./uploads')){
+				while(false !== ($fichier = readdir($dossier)))
+				{
+					if($fichier != '.' && $fichier != '..' && $fichier != 'ajout.jpeg' && $fichier != 'validation.png')
+					{
+						$im = pathinfo($fichier);
+						if(!isset($nomIm)){
+							$nomIm=$im['filename'];
+						}
+						else{
+							$nomIm=$nomIm.":".$im['filename'];
+						}
+						//array_push($listImgs, $nomIm);
+						// if($cpt>0){
+						// 	$cook=$_COOKIE["testcook"];
+						// 	$new=$cook."et".$nomIm;
+						// 	echo "<p>".$new."</p><br/>";
+						// 	setcookie("testcook",$new,time()+3600);
+						// }
+						// else{
+						// 	$cpt++;
+						// 	setcookie("testcook",$nomIm,time()+3600);
+						// }
+					}
+				}
+				/*echo "<pre>";
+				print_r ($listImgs);
+				echo "</pre>";*/
+				//$l = json_encode(serialize($listImgs));
+				//$l=str_replace("\"","'",$l);
+				/*echo "<pre>";
+				print_r ($l);
+				echo "</pre>";*/
 				if($dossier = opendir('./uploads')){
 					while(false !== ($fichier = readdir($dossier)))
 					{
 						if($fichier != '.' && $fichier != '..' && $fichier != 'ajout.jpeg' && $fichier != 'validation.png')
 						{
-							$ext = ".".strtolower(pathinfo($fichier,PATHINFO_EXTENSION));
-							echo "ajoutImage('".basename($fichier,$ext)."','uploads/".$fichier."');";
+							echo "<a href=\"pointsI.php?img=".basename($fichier)."&li=".$nomIm."\" ><img id=\"".basename($fichier)."\" class=\"photos rounded img-fluid\" src=\"./uploads/".basename($fichier)."\" alt=\"Désolé notre image a rencontré des problèmes\"></a>";
 							
 						}
 					}
 				}
-			?>
-		</script>
-		<button id="Ajout" class="btn btn-primary" onclick="ajoutDossier()">
-			<img class="imgBtn" src="images/ajout.jpeg" alt="Désolé notre image a rencontré des problèmes">
-		</button>
-
-		<button class="btn btn-success">
-			<img class="imgBtn" src="images/validation.png" alt="Désolé notre image a rencontré des problèmes">
-		</button>
+			}
+		?>
+	</section>
+	<footer>
+	<div class="d-flex justify-content-center">
+		<button class="btn btn-danger">Créer</button>
 	</div>	
 	</footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
--- AframeTourCreator/upload.php
+++ AframeTourCreator/upload.php
@@ -6,54 +6,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Aframe Tour Creator</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="tourCreator.css" />
+    <link type="text/css" rel="stylesheet" href="css/tourCreator.css" />
 </head>
 <body>
 <?php
-$c=count($_FILES["directoryToUpload"]["name"]);
-for($i=0;$i<$c;$i++){
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["directoryToUpload"]["name"][$i]); // nom sans extension
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    // extension de l'image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["directoryToUpload"]["tmp_name"][$i]); //retourne false si ce n'est pas une image
-        if($check !== false) {
-            $uploadOk = 1;
-        } else {
-            echo "Seules les images sont acceptées !!</br>";
+if(strcmp($_FILES["filesUpload"]["name"][0], "")!=0){
+    $c=count($_FILES["filesUpload"]["name"]);
+    for($i=0;$i<$c;$i++){
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["filesUpload"]["name"][$i]); // nom sans extension
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // extension de l'image
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["filesUpload"]["tmp_name"][$i]); //retourne false si ce n'est pas une image
+            if($check !== false) {
+                $uploadOk = 1;
+            } else {
+                echo "Seules les images sont acceptées !!</br>";
+                $uploadOk = 0;
+            }
+        }
+
+        if (file_exists($target_file)) {
+            echo "L'image existe déjà !</br>";
+            $uploadOk = 0;
+        } 
+
+           if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
+            echo "Seuls les formats jpg, jpeg et png sont acceptés</br>";
             $uploadOk = 0;
         }
-    }
 
-    if (file_exists($target_file)) {
-        echo "L'image existe déjà !</br>";
-        $uploadOk = 0;
-    } 
+        if ($uploadOk == 0) {
+            echo "Fichier non upload !</br>";
 
-       if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
-        echo "Seuls les formats jpg, jpeg et png sont acceptés</br>";
-        $uploadOk = 0;
-    }
-
-    if ($uploadOk == 0) {
-        echo "Fichier non upload !</br>";
-
-    } else {
-        if (move_uploaded_file($_FILES["directoryToUpload"]["tmp_name"][$i], $target_file)) // réalise l'upload.
-        {
-            echo "Le fichier ". basename( $_FILES["directoryToUpload"]["name"][$i]). " a été upload avec SUCCES</br>";
         } else {
-            echo "Erreur inconnue au bataillon</br>";
+            if (move_uploaded_file($_FILES["filesUpload"]["tmp_name"][$i], $target_file)) // réalise l'upload.
+            {
+                echo "Le fichier ". basename( $_FILES["filesUpload"]["name"][$i]). " a été upload avec SUCCES</br>";
+            } else {
+                echo "Erreur inconnue au bataillon</br>";
+            }
         }
     }
+    echo "<form action=\"accueil.php\">
+          <input type=\"submit\" value=\"Terminer\" />
+          </form>";
+}
+else{
+    echo "<p>Il faut sélectionner au minimum une image ...</p>";
+    echo "<form action=\"index.php\">
+          <input type=\"submit\" value=\"Retour\" />
+          </form>";
 }
 ?>
-
-<form action="accueil.php">
-    <input type="submit" value="Terminer" />
-</form>
 
 </body>
 </html>
